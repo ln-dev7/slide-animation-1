1dd00cca4fe6dc4a301939edcd8688f63dbf5a7a
LN <leonelngoya@gmail.com>
2024-04-28 10:31:32 +0100

    first version

--- main.js
+++ main.js
@@ -1,2 +1,39 @@
-import './style.scss'
-import gsap from 'gsap'
+import "./style.scss";
+import gsap from "gsap";
+
+const avatars = document.querySelectorAll(".avatar");
+const texts = document.querySelectorAll(".text");
+const images = document.querySelectorAll(".image");
+
+avatars.forEach((avatar, index) => {
+  avatar.addEventListener("click", () => {
+    gsap.to(texts, {
+      duration: 1,
+      y: `-${index * 100}%`,
+      opacity: 0,
+      ease: "power4.out",
+    });
+    gsap.to(texts[index], {
+      duration: 1,
+      opacity: 1,
+      ease: "power4.out",
+    });
+    gsap.to(avatars, {
+      duration: 1,
+      width: 96,
+      height: 96,
+      ease: "power4.out",
+    });
+    gsap.to(avatars[index], {
+      duration: 1,
+      width: 128,
+      height: 128,
+      ease: "power4.out",
+    });
+    gsap.to(images, {
+      duration: 1,
+      y: `-${index * 100}%`,
+      ease: "power4.out",
+    });
+  });
+});
